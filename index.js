4b5ec1cad68919a5c6425e9eadfbf9e375ba0ea1
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-05-10 11:52:57 +0530

    node express app initialization

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Express App');
+});
+
+app.listen(port, () => {
+  console.log('Server is running');
+});
